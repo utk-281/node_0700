5e4b6cd231f716a6b908dadc9cfb75b2ee475835
utkarsh <utkarshgupta.281@gmail.com>
2025-01-27 08:59:44 +0530

    add complete fs

--- Modules/Built In/fs.js
+++ Modules/Built In/fs.js
@@ -357,30 +357,137 @@ let fs1 = require("fs").promises;
 // fs1.mkdirSync("./BlogApp/backend/controller");
 // fs1.writeFileSync("./BlogApp/backend/controller/userController.js", "hi this is controller");
 
-fs1
-  .mkdir("./BlogApp")
-  .then(() => {
-    fs1
-      .mkdir("./BlogApp/backend")
-      .then(() => {
-        fs1
-          .mkdir("./BlogApp/backend/controller")
-          .then(() => {
-            fs1
-              .writeFile("./BlogApp/backend/controller/userController.js", "let a = 20")
-              .then(() => {})
-              .catch((err) => {
-                console.log(err);
-              });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// fs1
+//   .mkdir("./BlogApp")
+//   .then(() => {
+//     fs1
+//       .mkdir("./BlogApp/backend")
+//       .then(() => {
+//         fs1
+//           .mkdir("./BlogApp/backend/controller")
+//           .then(() => {
+//             fs1
+//               .writeFile("./BlogApp/backend/controller/userController.js", "let a = 20")
+//               .then(() => {})
+//               .catch((err) => {
+//                 console.log(err);
+//               });
+//           })
+//           .catch((err) => {
+//             console.log(err);
+//           });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+//! using async and await --> both are used in pairs, async is used in function declaration and await is used inside function body, we have to use await at statements where is is returning a promise.
+
+//! 1)  creating a folder
+
+async function createFile() {
+  let value = await fs.writeFile("./data.txt", "this is my data");
+  console.log(value);
+  console.log("file created");
+}
+
+// createFile();
+
+//! BlogApp --> backend --> controller --> userController.js (using async and await)
+
+async function abc() {
+  await fs.mkdir("./BlogApp");
+  console.log("BlogApp folder created");
+  await fs.mkdir("./BlogApp/backend");
+  console.log("backend created");
+  await fs.mkdir("./BlogApp/backend/controller");
+  console.log("controller created");
+  await fs.writeFile("./BlogApp/backend/controller/users.js", "let a = John");
+  console.log("file created");
+}
+// abc();
+
+//! whatever operations that we performed (sync/ async) it was not using streams
+
+let fs2 = require("fs");
+const { start } = require("repl");
+
+let data = fs2.readFileSync("./fs.js"); // source --> os (the file is present on my laptop)
+// console.log(data); // destination --> console (print it) // we are getting a buffer value
+/* <Buffer 2f 2f 21 20 63 6f 6e 73 0100001010001
+ 74 20 76 61 72 69 61 62 6c 65 
+4e 61 6d 65 20 3d 20 72 65 71 7
+5 69 72 65 28 27 70 61 74 68 27
+ 29 0d 0a 2f 2f 21 20 63 6f 6e 
+ 73 ... 12472 more bytes>
+
+
+!  NODEJS BUFFER -->
+! datatype of the output --> hexadecimal
+! originally it holds data in binary(0, 1) format
+! it is an array which is fixed in size
+*/
+// 2+2 ==> 010010 --> 1a (0901010) 0101010 => result 01010010101 (binary)
+// 1a e 1a == 2a
+// console.log(res) // ==> 4
+
+//! Buffer --> array of values which is not in readable format
+//~ we can manipulate the buffer values using it's methods
+
+// let fs3 = require("fs");
+
+// let value = fs3.createReadStream("./index.html", { highWaterMark: 50 });
+
+// value.on("data", (chunk) => {
+//   console.log(`this is my chunk --> ${chunk} -->  of size ${chunk.length}\n`);
+// });
+
+//! streaming ==> copying data from source to destination in continuous manner. In nodeJS we have 4 different types of streams
+
+// ==> 1) readable stream  --> this is used for reading data in continuous manner(chunks), method that we use is "createReadStream("path of the file")"
+
+// ==> 2) writable stream --> this is used for writing data in continuous manner, method name ==> createWriteStream("path/filename")
+
+// ==> 3) duplex stream --> in this we can perform reading/ writing at the same time (copy/pasting)
+
+// ==> 4) transform stream --> it is similar to duplex, but here we can modify the chunks
+
+let fs5 = require("fs");
+
+//! reading the contents in chunks
+// method name ==> createReadStream("path", "encoding")
+
+let value = fs5.createReadStream("./index.html", "utf-8"); // ==> createReadStream generate an event called "data"
+// console.log(value);
+
+// to catch/execute any event ==> on("the name of the event", cb)
+// to emit and event ==> emit("the name of the event")
+
+// value.on("data", (chunk) => {
+//   console.log(chunk);
+// });
+
+//! writing the contents in chunks
+// method name ==> createWriteStream("path/ filename")
+
+// let file = fs5.createWriteStream("./demo.java");
+// // console.log(file);
+
+// file.write("let a = 10", () => {
+//   console.log("file created");
+// });
+
+//! duplex stream ==> both operation at the same time
+// source --> index.html file
+// destination --> demo.java file
+
+let contents = fs5.createReadStream("./index.html", "utf-8");
+let newFile = fs5.createWriteStream("./demo.java");
+
+// to connect source and destination we use pipe()
+//! syntax ==> source.pipe(destination)
+contents.pipe(newFile);
