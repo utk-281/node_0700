e52e69a111d3a3b1f3868c1f3c1fcecc73a87da6
utkarsh <utkarshgupta.281@gmail.com>
2025-03-06 10:04:43 +0530

    add excalidraw file

--- Express/Blog Application/app.js
+++ Express/Blog Application/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { PORT } = require("./config/index");
 const { connectDB } = require("./config/db");
+const morgan = require("morgan");
 
 let userRoutes = require("./routes/user.routes");
 let blogRoutes = require("./routes/blog.routes");
@@ -12,6 +13,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.use("/v1/users", userRoutes);
 app.use("/v1/blogs", blogRoutes);
@@ -22,3 +24,5 @@ app.listen(process.env.PORT, (err) => {
   if (err) throw err;
   console.log("Express server listening on port:", PORT);
 });
+
+// C:\Program Files\nodejs\node_modules\npm\bin
--- Express/Blog Application/models/user.model.js
+++ Express/Blog Application/models/user.model.js
@@ -1,4 +1,5 @@
 let { Schema, model } = require("mongoose");
+let bcrypt = require("bcrypt");
 
 let userSchema = new Schema(
   {
@@ -29,6 +30,12 @@ let userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function () {
+  let salt = await bcrypt.genSalt(10); // generating a random string of size 10
+  let hashedPassword = await bcrypt.hash(this.password, salt); // hash the password with the salt
+  this.password = hashedPassword; // store the hashed password in database
+});
 
 module.exports = model("User", userSchema);
 // lowercase + plural ==> users
